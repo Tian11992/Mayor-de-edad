ea93b63417056c7def89884710ea67cd28b9cc8f
Sebastian Correa <sebas.11992@hotmail.com>
2019-09-03 14:47:32 -0500

    Programa para saber si una persona es mayor o menor de edad

---
+++ Edades/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Edades
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btningresar = new System.Windows.Forms.Button();
+            this.lbledad = new System.Windows.Forms.Label();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btningresar
+            // 
+            this.btningresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btningresar.Location = new System.Drawing.Point(438, 147);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(84, 31);
+            this.btningresar.TabIndex = 0;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.UseVisualStyleBackColor = true;
+            this.btningresar.Click += new System.EventHandler(this.btningresar_Click);
+            // 
+            // lbledad
+            // 
+            this.lbledad.AutoSize = true;
+            this.lbledad.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbledad.Location = new System.Drawing.Point(47, 147);
+            this.lbledad.Name = "lbledad";
+            this.lbledad.Size = new System.Drawing.Size(223, 31);
+            this.lbledad.TabIndex = 1;
+            this.lbledad.Text = "Ingrese su edad: ";
+            // 
+            // txtedad
+            // 
+            this.txtedad.Location = new System.Drawing.Point(299, 158);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(100, 20);
+            this.txtedad.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.lbledad);
+            this.Controls.Add(this.btningresar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btningresar;
+        private System.Windows.Forms.Label lbledad;
+        private System.Windows.Forms.TextBox txtedad;
+    }
+}
+
---
+++ Edades/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Edades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btningresar_Click(object sender, EventArgs e)
+        {
+            double edad = double.Parse(txtedad.Text);
+
+            if (edad >= 18)
+            {
+                MessageBox.Show("Es mayor de edad");
+            }
+            else
+            {
+                MessageBox.Show("No es mayor de edad");
+            }
+        }
+    }
+}
